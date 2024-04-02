4ad30086b261a77986e780c77fb6cbd11806f404
Nadissa Vela <122498954+nadissa1508@users.noreply.github.com>
2024-04-02 14:51:29 -0600

    Lógica inicial para manejar los nodos

--- Node.java
+++ Node.java
@@ -1,15 +1,88 @@
 public class Node <K,V>{
-    K pritority;
+
+    K priority;
     V infoPatient;
-    Node<K,V> izq;
-    Node<K,V> der;
-    Node<K,V> parent;
+    Node<K,V> Left;
+    Node<K,V> Right;
+    Node<K,V> Parent;
+
+    public K getPriority() {
+        return priority;
+    }
+
+    public void setPritority(K priority) {
+        this.priority = priority;
+    }
+
+    public V getInfoPatient() {
+        return infoPatient;
+    }
+
+    public void setInfoPatient(V infoPatient) {
+        this.infoPatient = infoPatient;
+    }
+
+    public Node<K, V> getLeft() {
+        return Left;
+    }
+
+    public void setLeft(Node<K, V> left) {
+        Left = left;
+    }
+
+    public Node<K, V> getRight() {
+        return Right;
+    }
+
+    public void setRight(Node<K, V> right) {
+        Right = right;
+    }
+
+    public Node<K, V> getParent() {
+        return Parent;
+    }
+
+    public void setParent(Node<K, V> parent) {
+        Parent = parent;
+    }
 
     public void EvaluateInvariantOrder(Node<K,V> root){
+        if(root.getLeft() == null && root.getRight() == null){
+            return;
+        }
 
+        if(root.getRight() != null){
+            if((root.getLeft().getPritority() < root.getPritority()) && (root.getRight().getPritority() < root.getPritority()) ){
+                if(root.getLeft().getPritority() < root.getRight().getPritority()){
+                    swap(root, root.getLeft());
+                    EvaluateInvariantOrder(root.getLeft());
+                }else{
+                    swap(root, root.getRight());
+                    EvaluateInvariantOrder(root.getRight());
+                }
+            }else{
+                if((root.getLeft().getPritority() < root.getPritority()) || (root.getRight().getPritority() < root.getPritority()) ){
+                    if(root.getLeft().getPritority() < root.getPriority()){
+                        swap(root, root.getLeft());
+                        EvaluateInvariantOrder(root.getLeft());
+                    }else{
+                        swap(root, root.getRight());
+                        EvaluateInvariantOrder(root.getRight());
+                    }
+                }else{
+                    return;
+                }
+            }          
+        }else{
+            return;
+        }
     }
 
     public void InsertInvariantOrder(Node<K,V> root){
+
+    }
+
+    public void swap(Node raiz, Node nuevaRaiz){
 
     }
 
