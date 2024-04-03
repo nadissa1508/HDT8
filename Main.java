efc46f965c6753acf498abe4c530828d94ae0ac3
Nadissa Vela <122498954+nadissa1508@users.noreply.github.com>
2024-04-02 18:07:11 -0600

    método main agregado

--- Main.java
+++ Main.java
@@ -1,3 +1,9 @@
-public class Main{
-    
+public class Main {
+    public static void main(String[] args) {
+        ReadFile reader = new ReadFile();
+        PriorityQueueTree patientsPriorityQueue = reader.readPatientsFile("pacientes.txt");
+        System.out.println("\n\nEMERGENCIAS");
+        System.out.println("\nLista de pacientes a atender:");
+        patientsPriorityQueue.showPatients();
+    }
 }
